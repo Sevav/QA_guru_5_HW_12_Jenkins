b1c38418e0678d9d3dc549f86e9de9b374e189a5
SegeiVasiltcov <tryaga1n@mail.ri>
2023-06-08 15:12:22 +0300

    add tests

--- test_demoqa.py
+++ test_demoqa.py
@@ -1,28 +1,10 @@
 import allure
 from selene import have, by
-from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from utils import attach
 
 
 @allure.title("Successful fill form")
-def test_successful():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": '100.0',
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
-    )
-    browser.config.driver = driver
+def test_successful(setup_browser):
+    browser = setup_browser
     first_name = "Alex"
     last_name = "Egorov"
 
@@ -58,7 +40,4 @@ def test_successful():
         # browser.element(".table-responsive").should(
         #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
 
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
+
